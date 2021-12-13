8fe7e62b0a0688abbc96b004c58a12cba544c2b5
dudu <dudu.du@kakaoenterprise.com>
2021-12-13 22:15:02 +0900

    [2021 CRYPTO TRACKER] Part4.4

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -27,7 +27,8 @@ const Coin = styled.li`
     a {
         padding: 20px;
         transition: color 0.2s ease-in;
-        display: block;
+        display: flex;
+        align-items: center;
     }
     &:hover {
         a {
@@ -44,6 +45,12 @@ font-size: 48px;
 const Loader = styled.div`
     text-align: center;
     display: block;
+`;
+
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -77,7 +84,13 @@ function Coins() {
                 (
                     <CoinsList>
                         {coins.map(coin => <Coin key={coin.id}>
-                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                            <Link to={{
+                                pathname: `/${coin.id}`,
+                                state: { name: coin.name },
+                            }}>
+                                <Img src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
+                                {coin.name} &rarr;
+                                </Link>
                         </Coin>)}
                     </CoinsList>
                 )}
