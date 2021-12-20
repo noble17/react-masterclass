366030c02e4aaec10665931c32288feb63c10f71
dudu <dudu.du@kakaoenterprise.com>
2021-12-20 10:29:34 +0900

    [2021 CRYPTO TRACKER] Part4.5

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,6 @@
 import { useLocation, useParams } from "react-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { stat } from "fs";
 
 interface RouteParams {
     coinId: string;
@@ -37,6 +36,16 @@ function Coin() {
     const [loading, setLoading] = useState(true);
     const { coinId } = useParams<RouteParams>();
     const { state } = useLocation<RouteState>();
+    const [info, setInfo] = useState({});
+    const [priceInfo, setPriceInfo] = useState({});
+    useEffect(() => {
+        (async () => {
+            const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+            const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+            setInfo(infoData);
+            setPriceInfo(priceData);
+        })();
+    }, [])
     return (
         <Container>
             <Header>
