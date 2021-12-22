16cbb5d6fe931b0113f43e78650f50d75498417c
dudu <dudu.du@kakaoenterprise.com>
2021-12-22 22:35:58 +0900

    [2021 CRYPTO TRACKER] Part4.8

--- src/App.tsx
+++ src/App.tsx
@@ -51,9 +51,12 @@ table {
   box-sizing: border-box;
 }
 body {
+	font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
   background-color: ${props => props.theme.bgColor};
-  color: ${props => props.theme.textColor}
+  color:${(props) => props.theme.textColor};
+  line-height: 1.2;
+
 }
 a {
   text-decoration: none;
@@ -62,11 +65,11 @@ a {
 `;
 
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <Router />
-    </>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<Router />
+		</>
+	);
 };
 export default App;
